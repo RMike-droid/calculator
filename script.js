328f86217adada63aa870743ab6961008a07933a
RMike-droid <galyenr@gmail.com>
2024-02-26 17:59:52 -0600

    Write basic operation functions

---
+++ script.js
@@ -0,0 +1,37 @@
+let numberOne = 0;
+let numberTwo = 0;
+let operator = undefined;
+
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(num1, opr, num2) {
+    switch(opr) {
+        case '+':
+            return add(a, b);
+            break;
+        case '-':
+            return subtract(a, b);
+            break;
+        case '*':
+            return multiply(a, b);
+            break;
+        case '/':
+            return divide(a, b);
+            break;
+    }
+}
